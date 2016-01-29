140e8d27e5276d8f93e7af48d23cdde373eceedf
Michael Brewer <mikeb@steelhouse.com>
2016-01-28 19:30:08 -0800

    switched to var to avoid node errors

--- autobind.js
+++ autobind.js
@@ -1,14 +1,13 @@
-
 module.exports = function (self) {
 	autobind(self, self.constructor.prototype);
 
 }
 
 function autobind(self, proto) {
-	for(let key of Object.getOwnPropertyNames( proto )){
+	for(var key of Object.getOwnPropertyNames( proto )){
 		if(key !== 'constructor') self[key] = self[key].bind(self);
 	}
-	let parentProto = Object.getPrototypeOf(proto);
+	var parentProto = Object.getPrototypeOf(proto);
 	if (parentProto !== Object.prototype){
 		autobind(self, parentProto);
 	}
