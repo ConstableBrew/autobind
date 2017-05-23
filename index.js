95ce7c7728372efc5680583b6a1cca82c7f325c8
ConstableBrew <constablebrew@gmail.com>
2017-05-22 23:27:04 -0700

    1.0.7

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ module.exports = function autobind(self) {
 
 function _autobind(self, proto) {
     var properties = Object.getOwnPropertyNames(proto);
-    for (var i; i < properties.length; ++i) {
+    for (var i = 0; i < properties.length; ++i) {
         var key = properties[i];
-        if (typeof self[key] === 'function' && key !== 'constructor') self[key] = self[key].bind(self);
+        if (typeof self[key] === 'function' && key !== 'constructor')
+            self[key] = self[key].bind(self);
     }
     var parentProto = Object.getPrototypeOf(proto);
     if (parentProto !== Object.prototype) {
